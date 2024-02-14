faa48e96cf57c0cd4ce0df69879ef418ce690b72
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-14 23:17:48 +0000

    first commit

---
+++ decimal_binary_hex.py
@@ -0,0 +1,25 @@
+"""
+Homework
+Task 1: Convert your first name and the initials of your surname into their ASCII hexadecimal
+and binary representations. Investigate online how to achieve this. Make sure to capitalise
+the first letter of your name and initials. Additionally, add a space character between your
+first-name and initials.
+
+Task 2: Pick a famous person and identify their year of birth (year between 1800-present).
+Convert this 4 digit decimal number into binary and hex formats. Use the methods
+described in the videos to achieve this.
+
+Format your answers as l have posted in the forum
+
+
+Solution
+ASCII chars: Andrii S
+ASCII hex: 41 6E 64 72 69 69 20 53
+ACSII binary: 01000001 01101110 01100100 01110010 01101001 01101001 00100000 01010011
+
+YOB — Kanō Jigorō (https://en.wikipedia.org/wiki/Kan%C5%8D_Jigor%C5%8D (founder of Judo)) — 1860
+1860/2 = 930 (0); 930/2 = 465 (0); 465/2 = 232 (1); 232/2 = 116 (0); 116/2 = 58 (0); 58/2 = 29 (0);
+29/2 = 14 (1); 14/2 = 7 (0); 7/2 = 3 (1); 3/2 = 1 (1); 1/2 = 0 (1)
+
+Binary: 0111 0100 0100 - Hex: B44
+"""
